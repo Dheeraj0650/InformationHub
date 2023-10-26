1c706d1e5d2dafaa2dc16b156f53b089cebac4ae
Dheeraj0650 <41461773+Dheeraj0650@users.noreply.github.com>
2023-10-26 08:54:31 -0600

    changed config file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,15 +1,30 @@
+// module.exports = {
+//     apps: [
+//       {
+//         name: "InformationHub",
+//         script: "npm",
+//         args: "run dev",
+//         interpreter: "none",
+//         autorestart: true,
+//         watch: false,
+//         max_memory_restart: "300M",
+//         log_date_format: "YYYY-MM-DD HH:mm Z"
+//       },
+//     ],
+//   };
+
 module.exports = {
-    apps: [
-      {
-        name: "InformationHub",
-        script: "npm",
-        args: "run dev",
-        interpreter: "none",
-        autorestart: true,
-        watch: false,
-        max_memory_restart: "300M",
-        log_date_format: "YYYY-MM-DD HH:mm Z"
-      },
-    ],
-  };
+  apps : [{
+    name: "My App",
+    script: "npm run dev",
+    instances: "max",
+    max_memory_restart: "256M",
+    env: {
+      NODE_ENV: "development"
+    },
+    env_production: {
+      NODE_ENV: "production"
+    }
+  }]
+};
   
