44a2a4ded60110708900f9dbcef33f4c0633ff23
Dheeraj0650 <41461773+Dheeraj0650@users.noreply.github.com>
2023-10-26 08:07:07 -0600

    changed config file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,28 +1,10 @@
-// module.exports = {
-//     apps: [
-//       {
-//         name: "InformationHub",
-//         script: "npm",
-//         args: "run dev"
-//       },
-//     ],
-//   };
-  
-  module.exports = {
-    apps : [{
-      name        : "demo-app",
-      script      : "npm start --prefix client",
-      watch       : true,
-      env: {
-        "NODE_ENV": "development",
+module.exports = {
+    apps: [
+      {
+        name: "InformationHub",
+        script: "npm",
+        args: "run dev"
       },
-      env_production : {
-         "NODE_ENV": "production"
-      }
-    },{
-      name       : "demo-cluster-app",
-      script     : "node app.js",
-      instances  : 2,
-      exec_mode  : "cluster"
-    }]
-  }
+    ],
+  };
+  
