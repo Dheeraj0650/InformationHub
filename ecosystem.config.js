e4bdf2ccd013d04be6291868fb1bf98fa161ec52
Dheeraj0650 <41461773+Dheeraj0650@users.noreply.github.com>
2023-10-26 10:33:44 -0600

    changed config file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,46 +1,56 @@
-// module.exports = {
-//     apps: [
-//       {
-//         name: "InformationHub",
-//         script: "npm",
-//         args: "run dev",
-//         interpreter: "none",
-//         autorestart: true,
-//         watch: false,
-//         max_memory_restart: "300M",
-//         log_date_format: "YYYY-MM-DD HH:mm Z"
-//       },
-//     ],
-//   };
-
 module.exports = {
-  apps : [{
-    name: "frontend",
-    script: "npm run client",
-    instances: "max",
-    max_memory_restart: "256M",
-    env: {
-      NODE_ENV: "development"
-    },
-    env_production: {
-      NODE_ENV: "production"
-    },
-    exec_mode:"cluster",
-    watch: true
-  },
-  {
-    name: "frontend",
-    script: "npm run server",
-    instances: "max",
-    max_memory_restart: "256M",
-    env: {
-      NODE_ENV: "development"
-    },
-    env_production: {
-      NODE_ENV: "production"
-    },
-    exec_mode:"cluster",
-    watch: true
-  }]
-};
+    apps: [
+      {
+        name: "Frontend",
+        script: "npm",
+        args: "run dev",
+        interpreter: "none",
+        autorestart: true,
+        watch: false,
+        max_memory_restart: "300M",
+        log_date_format: "YYYY-MM-DD HH:mm Z"
+      },
+      {
+        name: "Backend",
+        script: "npm",
+        args: "run server",
+        interpreter: "none",
+        autorestart: true,
+        watch: false,
+        max_memory_restart: "300M",
+        log_date_format: "YYYY-MM-DD HH:mm Z"
+      }
+    ],
+  };
+
+// module.exports = {
+//   apps : [{
+//     name: "frontend",
+//     script: "npm run client",
+//     instances: "max",
+//     max_memory_restart: "256M",
+//     env: {
+//       NODE_ENV: "development"
+//     },
+//     env_production: {
+//       NODE_ENV: "production"
+//     },
+//     exec_mode:"cluster",
+//     watch: true
+//   },
+//   {
+//     name: "backend",
+//     script: "npm run start",
+//     instances: "max",
+//     max_memory_restart: "256M",
+//     env: {
+//       NODE_ENV: "development"
+//     },
+//     env_production: {
+//       NODE_ENV: "production"
+//     },
+//     exec_mode:"cluster",
+//     watch: true
+//   }]
+// };
   
