759dc0a7d3231f072eec003ff8ec7194dc87a21c
Dheeraj0650 <41461773+Dheeraj0650@users.noreply.github.com>
2023-10-25 21:11:38 -0600

    changed ecosystem.config file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,13 +3,7 @@ module.exports = {
       {
         name: "InformationHub",
         script: "npm",
-        args: "run dev",
-        cwd: "/Users/kottedheeru/Documents/InformationHub",
-        interpreter: "none",
-        autorestart: true,
-        watch: false,
-        max_memory_restart: "300M",
-        log_date_format: "YYYY-MM-DD HH:mm Z",
+        args: "run dev"
       },
     ],
   };
