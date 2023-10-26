684cbb4fbd97e2ad80749c77256f43cd6e444e60
Dheeraj0650 <41461773+Dheeraj0650@users.noreply.github.com>
2023-10-26 10:21:28 -0600

    changed config file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -15,8 +15,22 @@
 
 module.exports = {
   apps : [{
-    name: "My App",
-    script: "npm run dev",
+    name: "frontend",
+    script: "npm run client",
+    instances: "max",
+    max_memory_restart: "256M",
+    env: {
+      NODE_ENV: "development"
+    },
+    env_production: {
+      NODE_ENV: "production"
+    },
+    exec_mode:"cluster",
+    watch: true
+  },
+  {
+    name: "frontend",
+    script: "npm run server",
     instances: "max",
     max_memory_restart: "256M",
     env: {
